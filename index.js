5b8bd94b367e8c344d3f98ff5bc0433ae84a638c
Jacob Mary Tapere <jacobmary.tapere21@gmail.com>
2024-06-16 15:07:12 +0800

    Final Changes.

--- index.js
+++ index.js
@@ -1,37 +1,29 @@
-var fName, lName, email, query, message;
+function sendEmail(fName, lName, email, query, message) {
+    var bodyMessage = `Full Name: ${fName} ${lName}<br>Email: ${email} <br> Message: ${message}`;
 
-var windowHeight = $(window).innerHeight();
-$('body').css({'height':windowHeight});
-
-
-$(".submit-button").on("click", function(event) {
-    event.preventDefault();
-    fName = $(".fName-input").val();
-    lName = $(".lName-input").val();
-    email = $(".email-input").val();
-    query = $("input[name='inquiry']:checked").val();
-    message = $(".message-textarea").val();
-    if(fName !== '' && lName !== '' && email !== '' && query !== '' && message !== '' ){
-        $("#toast-message").slideDown(1000).css({
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    paddingLeft: "40px" ,
-    borderRadius: "15px",
-    color: "white",
-        });
-        setTimeout(()=>{
-            $("#toast-message").slideUp(1000);
-        }, 3000);
-    }
-    else{
-        // submittedFunction();
-    }
-
-
-});
-
-
-var submittedFunction = () =>{
-    $("#contactForm")[0].reset();
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "jacobmary.tapere007@gmail.com",
+        Password : "79B0A934D8699E32E5A3E27E677CF4D352A9",
+        To : 'jacobmary.tapere007@gmail.com',
+        From : email,
+        Subject : query,
+        Body : bodyMessage,
+    }).then(
+        message => {
+            alert(message);
+            $("#toast-message").slideDown(1000).css({
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                paddingLeft: "40px" ,
+                borderRadius: "15px",
+                color: "white",
+            });
+            setTimeout(()=>{
+                $("#toast-message").slideUp(1000);
+            }, 3000);
+            $("#contactForm")[0].reset();
+        }
+    );
 }
